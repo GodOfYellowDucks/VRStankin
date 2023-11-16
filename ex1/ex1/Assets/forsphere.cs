35f8e07f035b27a92a1afe7587671212d65d615f
GodOfYellowDucks <69202189+GodOfYellowDucks@users.noreply.github.com>
2023-11-16 22:03:00 +0300

    exercise 1

---
+++ ex1/ex1/Assets/forsphere.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class forsphere : MonoBehaviour
+{
+    public GameObject capsule;
+    public Text text;
+    public int cntr;
+    public Button btnCntr;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cntr = 0;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        text.text = "Вы нажали " + cntr.ToString() + " раз";
+        if (cntr % 2 == 0) { capsule.gameObject.GetComponent<Renderer>().material.color = new Color32((byte)255, (byte)0, (byte)0, 1); }
+        if(cntr % 2 != 0) { capsule.gameObject.GetComponent<Renderer>().material.color = new Color32((byte)0, (byte)255, (byte)0, 1); }
+    }
+    public void btnClck()
+    {
+        cntr = cntr + 1;
+    }
+}
